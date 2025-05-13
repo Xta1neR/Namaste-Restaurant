a886b89f60b564f3b149b57a49ea8d78f863f15d
Rituraj <goswami.rituraj03@gmail.com>
2025-05-13 15:28:57 +0530

    fixes post changes

--- app/Menu/page.tsx
+++ app/Menu/page.tsx
@@ -4,10 +4,12 @@ import Header from '../components/header'
 import Footer from '../components/footer'
 import Reservation from '../components/reservation'
 import Menu from './components/menu'
+import PopupNotice from '../components/PopupNotice'
 
 function page() {
   return (
     <div className='bg-[#130f0c]'>
+      <PopupNotice />
       <Header />
       <Hero />
       <Menu />
